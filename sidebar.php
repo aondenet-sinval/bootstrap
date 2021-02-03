acee2ff1911dbffde017ab4706c72b69d5f1d265
Sinval <sinvalsatv@gmail.com>
2021-02-03 08:44:45 -0300

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,25 @@
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div id="img">
+  <img src="perf.jpg" class="rounded-circle" alt="Cinque Terre" width="304" height="400">    
+      </div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <h3>Some Links</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
